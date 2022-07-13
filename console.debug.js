220e1f72b77f2d2d2638feff5c94bc7fc92cd101
unknown <qq365947335@hotmail.com>
2022-07-13 14:36:05 +0800

    update

---
+++ console.debug.js
@@ -0,0 +1,19 @@
+// 用于隐藏 控制台输出 object 中的 [[prototype]], 如: console.debug({a:1, b: {c: 2}})
+console.debug = function() {
+    function clear(o) {
+        var obj = JSON.parse(JSON.stringify(o));
+        // [!] clone
+        if (obj && typeof obj === 'object') {
+            obj.__proto__ = null;
+            // clear
+            for (var j in obj) {
+                obj[j] = clear(obj[j]); // recursive
+            }
+        }
+        return obj;
+    }
+    for (var i = 0, args = Array.prototype.slice.call(arguments, 0); i < args.length; i++) {
+        args[i] = clear(args[i]);
+    }
+    console.log.apply(console, args);
+};
